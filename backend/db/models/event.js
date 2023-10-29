33ba906a2a2cd25e83753081939b893951fce514
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2023-10-29 08:57:38 -0400

    added eventimage and seeder errors

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Event.hasMany(
+      //   models.EventImage,
+      //   {
+      //     foreignKey: 'eventId'
+      //   }
+      // );
     }
   }
   Event.init({
---
+++ backend/db/seeders/20231029125435-demo-eventimage.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { EventImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await EventImage.bulkCreate([
+      {
+        eventId: 1,
+        url: "image url 1",
+        preview: true
+      },
+      {
+        eventId: 2,
+        url: "image url 2",
+        preview: true
+      },
+      {
+        eventId: 3,
+        url: "image url 3",
+        preview: true
+      }
+     ], { validate: true })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'EventImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ["image url 1", "image url 2", "image url 3"]}
+    })
+  }
+};
