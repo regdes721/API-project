84de571984a6d4c8cf01349e2d446ff4a053905f
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2023-10-30 09:29:46 -0400

    completed group by id local tests

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { Group, Membership, GroupImage } = require('../../db/models');
+const { Group, Membership, GroupImage, User, Venue } = require('../../db/models');
 const { Op } = require('sequelize');
 
 router.get('/', async (req, res) => {
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
     let groupsList = [];
 
     for (const group of groups) {
-        const groupData = group.toJSON()
+        const groupData = group.toJSON();
         groupData.numMembers = await Membership.count({
             where: {
                 groupId: group.id,
@@ -34,6 +34,56 @@ router.get('/', async (req, res) => {
     groupsBody["Groups"] = groupsList;
 
     res.json(groupsBody);
+});
+
+router.get('/:groupId', async (req, res) => {
+    const groupId = req.params.groupId;
+    const group = await Group.findOne({
+        where: {
+            id: groupId
+        }
+    });
+    if (group) {
+        const groupData = group.toJSON();
+        groupData.numMembers = await Membership.count({
+            where: {
+                groupId: group.id,
+                status: {
+                    [Op.not]: "pending"
+                }
+            }
+        });
+        groupData.GroupImages = await GroupImage.findAll({
+            where: {
+                groupId: group.id
+            },
+            attributes: ['id', 'url', 'preview']
+        });
+        groupData.Organizer = await User.findOne({
+            where: {
+                id: group.organizerId
+            },
+            attributes: ['id', 'firstName', 'lastName']
+        });
+        groupData.Venues = await Venue.findAll({
+            where: {
+                groupId: group.id
+            },
+            attributes: {
+                exclude: ['createdAt', 'updatedAt']
+            }
+        })
+
+        res.json(groupData)
+    }
+    if (!group) {
+        const err = new Error("Group couldn't be found");
+        err.status = 404;
+        res.json({
+            message: err.message
+        })
+        // next(err)
+    }
 });
 
 module.exports = router;
