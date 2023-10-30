b924238e3a3b67487721d32f3d91b725f5a05ba3
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2023-10-30 08:43:30 -0400

    completed Get all Groups local tests

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const router = express.Router();
-const { Group } = require('../../db/models');
+const { Group, Membership, GroupImage } = require('../../db/models');
+const { Op } = require('sequelize');
 
 router.get('/', async (req, res) => {
-    const groups = await Group.findAll()
+    const groups = await Group.findAll();
+    const groupsBody = {
+        "Groups": []
+    }
+
+    let groupsList = [];
+
+    for (const group of groups) {
+        const groupData = group.toJSON()
+        groupData.numMembers = await Membership.count({
+            where: {
+                groupId: group.id,
+                status: {
+                    [Op.not]: "pending"
+                }
+            }
+        });
+        groupImage = await GroupImage.findOne({
+            where: {
+                groupId: group.id,
+                preview: true
+            }
+        })
+        groupData.previewImage = groupImage.url
+        groupsList.push(groupData);
+    }
+
+    groupsBody["Groups"] = groupsList;
+
+    res.json(groupsBody);
 });
 
 module.exports = router;
