1972343254e66f743af3ac5b8f4f9754f2249175
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2023-11-01 19:40:07 -0400

    completed get events by group local tests

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { Group, Membership, GroupImage, User, Venue } = require('../../db/models');
+const { Group, Membership, GroupImage, User, Venue, Event, Attendance, EventImage } = require('../../db/models');
 const { Op } = require('sequelize');
 const { requireAuth, restoreUser } = require('../../utils/auth');
 
@@ -421,7 +421,66 @@ router.post('/:groupId/venues', requireAuth, restoreUser, async (req, res) => {
 });
 
 router.get('/:groupId/events', async (req, res) => {
-
+    const groupId = req.params.groupId;
+    let group = await Group.findByPk(groupId);
+    if (!group) {
+        const err = new Error("Group couldn't be found");
+        res.status(404);
+        // err.status = 404;
+        return res.json({
+            message: err.message
+        });
+        // next(err)
+    }
+    const events = await Event.findAll({
+        where: {
+            groupId
+        },
+        include: [
+            {
+                model: Group,
+                attributes: ['id', 'name', 'city', 'state']
+            },
+            {
+                model: Venue,
+                attributes: ['id', 'city', 'state']
+            }
+        ]
+    });
+    const eventsBody = {
+        "Events": []
+    }
+    let eventsList = [];
+    for (const event of events) {
+        const eventData = event.toJSON();
+        eventData.numAttending = await Attendance.count({
+            where: {
+                eventId: event.id,
+                status: "attending"
+            }
+        })
+        let eventImage = await EventImage.findOne({
+            where: {
+                eventId: event.id,
+                preview: true
+            }
+        });
+        if (eventImage) eventData.previewImage = eventImage.url;
+        const startDate = new Date(eventData.startDate);
+        const endDate = new Date(eventData.endDate);
+        const formattedStartDate = startDate.toISOString().replace('T', ' ').slice(0, 19);
+        const formattedEndDate = endDate.toISOString().replace('T', ' ').slice(0, 19);
+        eventData.startDate = formattedStartDate;
+        eventData.endDate = formattedEndDate;
+        delete eventData.description;
+        delete eventData.capacity;
+        delete eventData.price;
+        delete eventData.createdAt;
+        delete eventData.updatedAt;
+        eventsList.push(eventData);
+    }
+    eventsBody["Events"] = eventsList;
+    return res.json(eventsBody);
 });
 
 module.exports = router;
