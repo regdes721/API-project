6efea7b8a864b6371d7f4be9da4bec5c12cdda23
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2023-11-02 22:34:09 -0400

    completed delete group image local tests

--- backend/routes/api/groupimages.js
+++ backend/routes/api/groupimages.js
@@ -1,8 +1,55 @@
 const express = require('express')
 const router = express.Router();
-const { Event, Venue, EventImage, Attendance, Group, User, Membership } = require('../../db/models');
+const { GroupImage, Group, Membership } = require('../../db/models');
 const { Op } = require('sequelize');
 const { requireAuth, restoreUser } = require('../../utils/auth');
 const { route } = require('./groups');
+
+router.delete('/:imageId', requireAuth, restoreUser, async (req, res) => {
+    const imageId = req.params.imageId;
+    let groupId;
+    let groupImage = await GroupImage.findByPk(imageId);
+    if (!groupImage) {
+        const err = new Error("Group Image couldn't be found");
+        res.status(404);
+        // err.status = 404;
+        return res.json({
+            message: err.message
+        });
+        // next(err)
+    }
+    if (groupImage) groupId = groupImage.groupId;
+    const groupOrganizer = await Group.findOne({
+        where: {
+            id: groupId,
+            organizerId: req.user.id
+        }
+    });
+    const groupCoHost = await Group.findOne({
+        include: {
+            model: Membership,
+            where: {
+                userId: req.user.id,
+                status: "co-host"
+            }
+        },
+        where: {
+            id: groupId
+        }
+    });
+    if (!groupOrganizer && !groupCoHost) {
+        const err = new Error("Forbidden");
+        res.status(403);
+        // err.status = 403;
+        return res.json({
+            message: err.message
+        });
+        // next(err);
+    }
+    await groupImage.destroy();
+    return res.json({
+        "message": "Successfully deleted"
+      });
+})
 
 module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,6 +8,7 @@ const usersRouter = require('./users.js');
 const groupsRouter = require('./groups.js');
 const venuesRouter = require('./venues.js');
 const eventsRouter = require('./events.js');
+const groupImagesRouter = require('./groupimages.js');
 
 router.use(restoreUser);
 
@@ -20,6 +21,8 @@ router.use('/groups', groupsRouter);
 router.use('/venues', venuesRouter);
 
 router.use('/events', eventsRouter);
+
+router.use('/group-images', groupImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
